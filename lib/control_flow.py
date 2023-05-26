5e892ba61d7a64f6daadb319209db8ba687d1939
Zoe Guidry <oyezoey@gmail.com>
2023-05-25 21:32:52 -0500

    lab completed.

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,45 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
+    if username == 'admin' or username == 'ADMIN':
+        return 'Access granted' if password == '12345' else 'Access denied'
+    else:
+        return 'Access denied'
     # your code here
     pass
 
 def hows_the_weather(temperature):
-    # your code here
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature < 65:
+        return "It's a little chilly out there!"
+    elif temperature < 85:
+        return "It's perfect out there!"
+    else:
+        return "It's too dang hot out there!"
     pass
 
 def fizzbuzz(num):
-    # your code here
+    if num % 3 == 0 and num % 5 == 0:
+        return 'FizzBuzz'
+    elif num % 3 == 0:
+        return 'Fizz'
+    elif num % 5 == 0:
+        return 'Buzz'
+    else:
+        return num
     pass
 
 def calculator(operation, num1, num2):
-    # your code here
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        return num1 / num2
+    else:
+        print('Invalid operation!')
+        return None
     pass
